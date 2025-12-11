4934e4c695fa3468ec7d422a7c05aa72db731341
krbynl <107776046+krbynl@users.noreply.github.com>
2025-12-10 22:08:08 -0500

    Create page.tsx

---
+++ app/voice/page.tsx
@@ -0,0 +1,9 @@
+import { VoiceChat } from '@/components/voice-chat';
+
+export default function VoiceChatPage() {
+  return (
+    <div className="h-screen w-full">
+      <VoiceChat className="h-full" />
+    </div>
+  );
+}
