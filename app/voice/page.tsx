ee35b96875017c8e8e9a3a75bd7ba5a85fd40e01
krbynl <107776046+krbynl@users.noreply.github.com>
2026-01-16 15:44:17 -0500

    Update page.tsx

--- app/voice/page.tsx
+++ app/voice/page.tsx
@@ -1,9 +1,6 @@
 'use client';
 
-import { useEffect } from 'react';
 import { VoiceChat } from '@/components/voice-chat';
-
-'use client';
 
 export default function VoiceChatPage() {
   // Service Worker removed - not needed yet
